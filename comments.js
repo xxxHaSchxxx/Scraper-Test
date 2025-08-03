ccb23aa414732d667e3757989d9882cdd62e178e
xxxHaSchxxx <hannesscheuren@web.de>
2025-08-03 13:31:15 +0200

    Update comments.js

--- comments.js
+++ comments.js
@@ -2,7 +2,9 @@ const comments = [
   "Willkommen zur Testseite!",
   "Kommentar hinzugefügt am 3. August 2025",
   "Dies ist ein Beispiel für eine Änderung.",
-  "Änderung 1"
+  "Änderung 1",
+  "Test 2"
 ];
+
 
 
